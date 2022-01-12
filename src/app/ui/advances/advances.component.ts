7952467ff17864a818a5f1e42436f65e281d5357
Alexander Morland <alek.morland@gmail.com>
2022-01-12 23:35:59 +0100

    more pretty advance list

--- src/app/ui/advances/advances.component.ts
+++ src/app/ui/advances/advances.component.ts
@@ -1,3 +1,4 @@
+import { AdvancesService } from 'src/app/services/advances.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvancesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public advancesService: AdvancesService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
--- src/app/ui/advances/card/card.component.ts
+++ src/app/ui/advances/card/card.component.ts
@@ -2,6 +2,21 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Advance } from 'src/app/interfaces/advance';
 import { AdvancesService } from 'src/app/services/advances.service';
 
+
+type Sym = {
+  [key:string] : string
+}
+
+const symbols:Sym = {
+  exploration: '✬',
+  religion: '✞',
+  communications: '✍',
+  science: '✌',
+  civics: '➶',
+  commerce: '☞',
+};
+
+
 @Component({
   selector: 'aor-adv-card',
   templateUrl: './card.component.html',
@@ -18,10 +33,15 @@ export class CardComponent implements OnInit {
 
   constructor(private service: AdvancesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.advances = this.service.allByCategory(this.category);
     this.advances.forEach( a => this.total += a.cost );
     this.researchable = this.advances[0].researchable;
   }
 
+  public symbol(category: string): string
+  {
+    return symbols[category];
+  }
 }
